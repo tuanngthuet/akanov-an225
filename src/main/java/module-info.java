5b05f3c4e9a081db66aaf32837b8dd2af3bd2cba
tuanngthuet <24020347@vnu.edu.vn>
2025-10-16 21:34:31 +0700

    brick

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.demo2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.almasb.fxgl.all;
-
-    opens org.example.demo2 to javafx.fxml;
-    exports org.example.demo2;
-}
