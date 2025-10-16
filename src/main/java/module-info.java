cd98bff34726133f9a3dbb96f31bbb34e66a3b6a
huyle <huylekq11@gmail.com>
2025-10-16 11:33:50 +0700

    -Setup project structure, config game module -Create new folders due to MVC design pattern

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module gameplay {
+module view {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
 
-    opens gameplay to javafx.fxml;
-    exports gameplay;
+    opens view to javafx.fxml;
+    exports view;
 }
