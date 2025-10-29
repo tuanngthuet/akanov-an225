b9e7c832ea01a962c17656bb9c8b06e034f778e4
phamquanganh23 <24020023@vnu.edu.vn>
2025-10-30 00:02:43 +0700

    change
    đổi hàm va chạm

--- src/main/java/controller/ball_control/BallVari.java
+++ src/main/java/controller/ball_control/BallVari.java
@@ -1,7 +1,27 @@
 package controller.ball_control;
 
+import javafx.scene.image.Image;
+
+import java.util.Objects;
+
 public interface BallVari {
     double BALL_RADIUS = 12;
     double MAX_ANGLE = 70;
     double BALL_HITBOX = 10;
+
+    Image NORMAL_BALL = new Image(
+            Objects.requireNonNull(BallVari.class.getResource("/assets/textures/balls/ball_normal.png").toExternalForm())
+    );
+
+    Image HARD_BALL = new Image(
+            Objects.requireNonNull(BallVari.class.getResource("/assets/textures/balls/ball_hard.png").toExternalForm())
+    );
+
+    Image POWERUP_BALL = new Image(
+            Objects.requireNonNull(BallVari.class.getResource("/assets/textures/balls/ball_powerup.png").toExternalForm())
+    );
+
+    Image SPEEDUP_BALL = new Image(
+            Objects.requireNonNull(BallVari.class.getResource("/assets/textures/balls/ball_speedup.png").toExternalForm())
+    );
 }
